c5f85ec7af84da8368759fd583d41d8e437d2c69
sakshi123-code <sakshinaukudkar7@gmail.com>
2024-08-01 21:40:27 -0700

    array

---
+++ array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i;
+    int age[]={18,18,17,19,17,19};
+    for(i=0;i<5;i++)
+    {
+        printf("%d\n",age[i]);
+    }
+    return 0;
+
+
+}
+
