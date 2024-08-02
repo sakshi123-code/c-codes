844d74a799a93e30d6f259a465a89617eec1b19f
sakshi123-code <sakshinaukudkar7@gmail.com>
2024-08-01 21:42:05 -0700

    break

---
+++ break.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i;
+    for(int i=0;i<10;i++){
+    if(i==4)
+    {
+        break;
+
+    }
+    printf("%d\n",i);
+    } 
+return 0;
+}   
