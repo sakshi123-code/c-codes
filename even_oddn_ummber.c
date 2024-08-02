973f5dbecbb80131094fe0e64667a82a4c9aee70
sakshi123-code <sakshinaukudkar7@gmail.com>
2024-08-01 21:51:30 -0700

    even

---
+++ even_oddn_ummber.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("enter a number");
+scanf("%d",&num);
+if(num/2==0){
+printf("given num is even");    
+}
+else
+{
+printf("given num is odd");
+}
+return 0;
+
+}    
+
+
