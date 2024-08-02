3eade676a44972dcddb9ab9c89cdb150abb9dd89
sakshi123-code <sakshinaukudkar7@gmail.com>
2024-08-01 21:41:11 -0700

    countdown

---
+++ countdown.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int countdown=3;
+while(countdown>0)
+{
+printf("%d\n",countdown);
+countdown--;
+}
+printf("happy nwe year");
+return 0;
+}        
