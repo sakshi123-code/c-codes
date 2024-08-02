0a0e2bf59615dc21fc8c9f202cd81378df459b44
sakshi123-code <sakshinaukudkar7@gmail.com>
2024-08-01 21:39:09 -0700

    bubbal sort

---
+++ floating.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float x,y,p;
+printf("enter two numbers");
+scanf("%f%f",&x,&y);
+p=x*y;
+printf("the product of two numbers is%f",p);
+return 0;
+}
