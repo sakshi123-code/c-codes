9a737f4afab4517e589ec80286fe0b84ab03b3e5
sakshi123-code <sakshinaukudkar7@gmail.com>
2024-08-01 21:49:50 -0700

    whether

---
+++ whether.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i=2,n;
+printf("enter a number");
+scanf("%d",&n);
+while("n%i" !=0)
+{
+i++;
+}
+if(n==i)
+{
+printf(" prime number");
+}
+else
+{
+printf("not prime number");
+}
+getch();
+}
